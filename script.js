eb9e828d25e16edc9518d1a94c1c481f0c4fd516
Luiza Moreno <145570285+luizaamoreno@users.noreply.github.com>
2024-09-02 01:39:02 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -15,34 +15,27 @@ async function buscarFilmes() {
             
         </div>
         <img class="img_card" src="https://image.tmdb.org/t/p/w500${filme_da_vez.poster_path}" alt="foto do ${filme_da_vez.title}">`
-        // <div class="card_infos">
-        //     <h2 class="nome_card">Nome: <span class="destaque">${filme_da_vez.title}</span></h2>
-        //     <p class="texto_card">Espécie: <span class="destaque">${filme_da_vez.species}</span></p>
-        //     <p class="texto_card">Status: <span class="destaque">${filme_da_vez.status}</span></p>
-        //     <p class="texto_card">Localização: <span class="destaque">${filme_da_vez.location.name}</span></p>
-        // </div>
-        // `
         cards.appendChild(novo_card)
     })
 }
 buscarFilmes()
 
 
+const inputSearch = document.querySelector("#input_search")
 
-const nome = document.querySelector(".input_search")
-
-
-nome.addEventListener("input", async () => {
-    const resposta = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=77c4e2b070a2e1396500d0b42ebf7cec&language=pt-BR${nome.value}`)
+inputSearch.addEventListener("input", async () => {    
+    const resposta = await fetch(`https://api.themoviedb.org/3/search/movie?query=${inputSearch.value}&api_key=77c4e2b070a2e1396500d0b42ebf7cec&language=pt-BR`)
     const dados = await resposta.json()
     const lista_de_filmes = dados.results
+    
+    cards.innerHTML = ""
 
-    container.innerHTML = ""
 
     lista_de_filmes.forEach((filme_da_vez) => {
         const novo_card = document.createElement("div")
         novo_card.className = "card"
         novo_card.innerHTML = `
+        
         <div class="card_hover">
             <h3>${filme_da_vez.title}</h3>
             <p>${filme_da_vez.overview}</p>
@@ -51,5 +44,6 @@ nome.addEventListener("input", async () => {
         <img class="img_card" src="https://image.tmdb.org/t/p/w500${filme_da_vez.poster_path}" alt="foto do ${filme_da_vez.title}">`
         cards.appendChild(novo_card)
     })
+    buscarFilmes()
 
 })
